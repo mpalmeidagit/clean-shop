0af27e04986b4f17c26fbee3657f4c6f46412c77
Marivaldo Pereira de Almeida <marivaldo.almeida-ext@imperio.com.br>
2026-09-25 08:10:30 -0300

    Clean Shop - Criado estrutura do domain
    Adicionado entidade customer, configure services, ICustomer

---
+++ CleanShop/CleanShop.Domain/Interface/ICustomer.cs
@@ -0,0 +1,12 @@
+﻿using CleanShop.Domain.Entity;
+
+namespace CleanShop.Domain.Interface;
+
+public interface ICustomersDomain
+{
+    Task<bool> InsertAsync(Customer customer);
+    Task<bool> UpdateAsync(Customer customer);
+    Task<bool> DeleteAsync(string customerId);
+    Task<Customer> GetAsync(string customerId);
+    Task<IEnumerable<Customer>> GetAllAsync();
+}
